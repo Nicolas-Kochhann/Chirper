55da69dba04ea9afd9cb004297b9365efcd3d1da
Nicolas <kochhann.nicolass.soldi@gmail.com>
2026-01-18 14:14:32 -0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,53 @@
+<?php
+
+use App\Http\Controllers\Auth\Login;
+use App\Http\Controllers\Auth\Logout;
+use App\Http\Controllers\Auth\Register;
+use App\Http\Controllers\ChirpController;
+use Faker\Guesser\Name;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', [ChirpController::class,'index']);
+
+
+Route::middleware('auth')->group(function () {
+    Route::post('/chirps', [ChirpController::class,'store']);
+    Route::get('/chirps/{chirp}/edit', [ChirpController::class, 'edit']);
+    Route::put('/chirps/{chirp}', [ChirpController::class,'update']);
+    Route::delete('/chirps/{chirp}', [ChirpController::class,'destroy']);
+});
+
+/*
+
+Same behavior as the snippet above
+
+Route::resource('/chirps', ChirpController::class)
+    ->only('store', 'update', 'edit', 'delete');
+
+*/
+
+// REGISTER ROUTES
+
+Route::view('/register', 'auth.register')
+    ->middleware('guest')
+    ->name('register');
+
+Route::post('/register', Register::class)
+    ->middleware('guest');
+
+
+// LOGOUT ROUTES
+
+Route::post('/logout', Logout::class)
+    ->middleware('auth');
+
+
+// LOGIN ROUTES
+
+Route::view('/login', 'auth.login')
+    ->middleware('guest')
+    ->name('login');
+
+Route::post('login', Login::class)
+    ->middleware('guest');
+
